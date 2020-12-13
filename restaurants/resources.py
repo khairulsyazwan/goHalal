05e403d51b3c0f165a52dafe8b2c67e34d12eb0d
Mohd Saiful <mohd.saiful@live.com>
2020-12-14 06:12:52 +0800

    added get all restaurants endpoint

--- restaurants/resources.py
+++ restaurants/resources.py
@@ -2,6 +2,15 @@ from import_export import resources
 from .models import Restaurant
 
 class RestaurantResource(resources.ModelResource):
+
+    # imported_names = set()
+
+    # def after_import_row(self, row, row_result, row_number=None, **kwargs):
+    #     self.imported_names.add(row.get("id"))
+
+    # def skip_row(self, instance, original):
+    #     return instance.id in self.imported_names
+
     class Meta:
         model = Restaurant
         exclude = ['average_rating', 'is_registered', 'max_pax_reservations', 'capacity']
