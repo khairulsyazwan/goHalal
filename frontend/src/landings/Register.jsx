5e6fa6b34dad24c13f47bdf9c6e351bef16d65b3
hfzgfr <hfzgfr@gmail.com>
2020-12-14 13:36:18 +0800

    commiting this in order to pull master

--- frontend/src/landings/Register.jsx
+++ frontend/src/landings/Register.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-// import {render} from 'react-dom';
-import { Box, Button, Grid, Paper, TextField, FormControlLabel } from '@material-ui/core';
-import { Switch } from 'formik-material-ui';
+import { Box, Button, Grid, Paper, TextField } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-// import Axios from 'axios';
-import { Formik, Form, Field } from "formik";
-// import * as Yup from "yup";
+import Axios from 'axios';
+import * as Yup from "yup";
 
 
 
-// Material Theme
-
+// Material UI Theme
 const useStyles = makeStyles((theme) => ({
 grid: {
   width: "100%",
@@ -19,8 +15,8 @@ grid: {
 },
 paper: {
   padding: theme.spacing(5),
-  // textAlign: "center",
   color: theme.palette.primary.light,
+  // textAlign: "center",
   // background: "#303030",
   }
 }));
@@ -28,59 +24,6 @@ paper: {
 const Register = () => {
   const classes = useStyles();
 
-  // <Formik
-  //   iniialValues={{
-  //     username: '',
-  //     email: '',
-  //     password: '',
-  //   }}
-  //   validate={(values) => {
-  //     const errors = {};
-  //     if (!values.email) {
-  //       errors.email = 'Required';
-  //     } else if (
-  //       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  //     ) {
-  //       errors.email = 'Invalid email address';
-  //     }
-  //     return errors;
-  //   }}
-  //   onSubmit={(values, {setSubmitting}) => {
-  //     setTimeout(() => {
-  //       setSubmitting(false);
-  //       alert(JSON.stringify(values, null, 2))
-  //     }, 500);
-  //   }}>
-
-  // {({submitForm, isSubmitting}) => (
-  //       <Form>
-  //         <Box margin={1}>
-  //           <Field
-  //             name="email"
-  //             type="email"
-  //             label="Email"
-  //             helperText="Please Enter Email"
-  //           />
-  //         </Box>
-  //         <Box margin={1}>
-  //           <Field
-  //             component={TextField}
-  //             type="password"
-  //             label="Password"
-  //             name="password"
-  //           />
-  //         </Box>
-  //         <Box margin={1}>
-  //           <FormControlLabel
-  //             control={
-  //               <Field component={Switch} type="checkbox" name="rememberMe" />
-  //             }
-  //             label="Remember Me"
-  //           />
-  //         </Box>
-  //       </Form>
-  // )}
-  //   </Formik> 
   return (
         <>
           <Box m={8}>
