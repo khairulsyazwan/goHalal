22a8a6013933c852512d83236e097b91e62cb22d
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-12-14 18:49:39 +0800

    api tested

--- frontend/src/pages/Restaurant.jsx
+++ frontend/src/pages/Restaurant.jsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useParams, Redirect } from "react-router-dom";
 import axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 function Restaurant() {
-  const [single, setSingle] = useState([]);
+  const [single, setSingle] = useState({});
+  const [isLoaded, setisLoaded] = useState(false);
   let { id } = useParams();
 
   useEffect(() => {
+    getRestaurant();
     return () => {};
   }, []);
 
-  async function getRestaurants() {
+  async function getRestaurant() {
     try {
-      let resp = await axios.get("http://localhost:8000/api/v1/restaurants/");
-      let rest = resp.data.restaurants;
-      //   let arr = locations;
-      //   for (let index = 0; index < rest.length; index++) {
-      //     let holding = [];
-      //     holding.push(rest[index].name);
-      //     holding.push(rest[index].lat);
-      //     holding.push(rest[index].lng);
-      //     holding.push(rest[index].id);
-      //     holding.push(rest[index].address);
-      //     holding.push(rest[index].picture);
-      //     arr.push(holding);
-      //   }
-      //   setLocations(arr);
+      let resp = await axios.get(
+        `http://localhost:8000/api/v1/restaurants/${id}`
+      );
+      let info = resp.data.restaurant;
+      // console.log(rest);
+      setSingle({ info });
+      setisLoaded(true);
+      console.log(single);
     } catch (err) {
       console.log(err.response);
     }
   }
 
-  return <div>Restaurant Page</div>;
+  function renderPage() {
+    return (
+      <>
+        <h1>{single.info.name}</h1>
+        <h2>{single.info.cuisine}</h2>
+        <h1>{single.info.address}</h1>
+        <img src={single.info.picture} alt="" srcset="" />
+      </>
+    );
+  }
+
+  return isLoaded ? renderPage() : <CircularProgress />;
 }
 
 export default Restaurant;
