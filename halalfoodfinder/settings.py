267dbc1bf2d0663612d45ac4b8bef511b6380863
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-12-22 21:33:47 +0800

    test

--- halalfoodfinder/settings.py
+++ halalfoodfinder/settings.py
@@ -111,15 +111,18 @@ WSGI_APPLICATION = 'halalfoodfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': BASE_DIR / 'db.sqlite3',
-#     # }
-#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gohalal_development',
+        'USER': 'khairulsyazwan',  # postgres
+        # 'PASSWORD' : '',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
 
-DATABASES['default'] =  dj_database_url.config()
+
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
@@ -178,3 +181,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DATABASES['default'] =  dj_database_url.config()
