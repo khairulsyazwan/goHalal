c9005c4b832b266a8b47f0ebfebffe358a73d0fb
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-12-22 20:20:23 +0800

    test 4

--- halalfoodfinder/settings.py
+++ halalfoodfinder/settings.py
@@ -88,25 +88,19 @@ ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'halalfoodfinder.urls'
 
 TEMPLATES = [
-    # {
-    #     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    #     'DIRS': [],
-    #     'APP_DIRS': True,
-    #     'OPTIONS': {
-    #         'context_processors': [
-    #             'django.template.context_processors.debug',
-    #             'django.template.context_processors.request',
-    #             'django.contrib.auth.context_processors.auth',
-    #             'django.contrib.messages.context_processors.messages',
-    #         ],
-    #     },
-    # },
     {
-        'BACKEND':
-        'django.template.backends.django.DjangoTemplates',
-        'DIRS':
-        [os.path.join(BASE_DIR, 'build')]
-    }
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'halalfoodfinder.wsgi.application'
