ed5bcf87ef6351545f863c66d72eb64ada5e8029
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-12-22 20:38:59 +0800

    db

--- halalfoodfinder/settings.py
+++ halalfoodfinder/settings.py
@@ -110,9 +110,17 @@ WSGI_APPLICATION = 'halalfoodfinder.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gohalal_development',
+        'USER': 'khairulsyazwan',  # postgres
+        # 'PASSWORD' : '',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
